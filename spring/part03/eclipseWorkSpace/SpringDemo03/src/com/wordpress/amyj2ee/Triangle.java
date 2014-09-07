7a3108810ee0319f16b36f394cfcbed10f7423d6
Amruta <Amruta@DiyaaHomePC>
2014-09-07 13:40:25 -0400

    Spring initialize a property of an object by specifying the value in the configuration XML.

---
+++ spring/part03/eclipseWorkSpace/SpringDemo03/src/com/wordpress/amyj2ee/Triangle.java
@@ -0,0 +1,25 @@
+package com.wordpress.amyj2ee;
+
+public class Triangle {
+	
+	String type;
+	
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw(){
+		System.out.println("Triangle Drawn type is "+type);
+	}
+
+}
