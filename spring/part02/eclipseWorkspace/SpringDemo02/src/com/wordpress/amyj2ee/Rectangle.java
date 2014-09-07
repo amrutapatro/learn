00f8f7048f1e982c77a64bf85a1381064de479e9
Amruta <Amruta@DiyaaHomePC>
2014-09-07 13:37:56 -0400

    Replacing the above BeanFactory with the more powerful ApplicationContext.

---
+++ spring/part02/eclipseWorkspace/SpringDemo02/src/com/wordpress/amyj2ee/Rectangle.java
@@ -0,0 +1,9 @@
+package com.wordpress.amyj2ee;
+
+public class Rectangle {
+	
+	public void draw(){
+		System.out.println("Rectangle Drawn");
+	}
+
+}
---
+++ spring/part02/eclipseWorkspace/SpringDemo02/src/com/wordpress/amyj2ee/Triangle.java
@@ -0,0 +1,9 @@
+package com.wordpress.amyj2ee;
+
+public class Triangle {
+	
+	public void draw(){
+		System.out.println("Triangle Drawn");
+	}
+
+}
